b639fbf65e99512f681e2e2225d7af4fba5c6177
Eunbi (Emily) Go <eunbigo91@gmail.com>
2022-11-29 19:01:28 -0600

    search_function_and_read_csv

--- Extension/front-end/script.js
+++ Extension/front-end/script.js
@@ -1,8 +1,13 @@
-$(function() { 
+$('#inputText').change(function () {
+    $('#result').empty();
+    search_func($('#inputText').val());
+  });
+  
+function search_func(query) {
     $('#result').CSVToTable('back-end/output/sentiment_output.csv',{
-      tableClass: "result_table",
-      thClass: "table_header",
-      trClass: "table_row",
-      tdClass: "table_cell"
-    });
-});
+        tableClass: "result_table",
+        thClass: "table_header",
+        trClass: "table_row",
+        tdClass: "table_cell"
+      });
+}
