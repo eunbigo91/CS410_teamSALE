8ad97155577982561b967e5935d455d930b3f779
Sruthi Jayanti <sjayan3@illinois.edu>
2022-12-05 08:53:40 -0600

    made some temp changes to script file

--- Extension/front-end/script.js
+++ Extension/front-end/script.js
@@ -6,19 +6,24 @@ $('#inputText').change(function () {
 
 function search_func(query) {
     $.getJSON("back-end/output/final_mapping.json", function(json) {
+      // process query to all lower 
+      query = query.toLowerCase()
       var results = [];
       var searchVal = query;
       
       // searching for title. if found, pass asin to other json file to find sentiment label
       for (var i=0 ; i < json.length ; i++) {
-          if (json[i]["title"] == searchVal) {
+          title = json[i]["title"];
+          title = String(title).toLowerCase();
+          if (title.includes(searchVal)) {
             
             // push title to results array
             if (results.includes(json[i]["title"]) == false) {
-              results.push(json[i]["title"])
+              results.push(json[i]["title"]);
+              asin = json[i]["asin"];
             }
 
-            asin = json[i]["asin"]
+            
 
             // look for sentiment value based on asin
             $.getJSON("back-end/output/sentiment_output.json", function(json2) {
@@ -28,23 +33,29 @@ function search_func(query) {
                 if (json2[i]["asin"] == asin) {
                   if (results.includes(json2[i]["label"]) == false) {
                     results.push(json2[i]["label"]);
-                  }
-                  
-                  // building the final output string
-                  var sentiment_string = ""
+                  }                  
+                } 
+                
+                var final = "";
+                // building the final output string
+                if (!results.length) {
+                  var sentiment_string = "";
 
                   if (results[1] == "pos") {
-                    sentiment_string = "positive";
+                  sentiment_string = "positive";
 
                   } else if (results[1] == "neg") {
-                    sentiment_string = "negative";
+                  sentiment_string = "negative";
                   } else {
-                    sentiment_string = "neutral";
+                  sentiment_string = "neutral";
                   }
 
-                  var final = "Based on many reader's reviews, the overall sentiment around the novel " + results[0] + " is " + sentiment_string + ".";
-                  $('#result').html((final));
+                  final = "Based on many reader's reviews, the overall sentiment around the novel " + results[0] + " is " + sentiment_string + ".";
+                } else {
+                  final = "No results were found for that title";
                 }
+
+                $('#result').html(results);
               }
             });
          }
